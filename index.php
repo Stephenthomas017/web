98627e379d0df0f4ffac2635688e95d1edeaf5e2
Stephenthomas017 <62503790+Stephenthomas017@users.noreply.github.com>
2020-06-01 19:39:58 +0530

    added rentit homepage

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+  </head>
+  <body>
+   <div class="container">
+     <div class="header">
+       <div class="logo">
+        <a href="index.html" class="logo-text">rentitt</a>
+       
+        <ul class="navigation-menu">
+          <a href="#"><i class="fas fa-home home"></i> Home</a>
+          <a href="#"><i class="fas fa-align-left about"></i> About</a>
+          
+          <a href="#"><i class="fas fa-headset contact"></i> Contact</a>
+        </ul>
+       </div>
+       <div class="login">
+        <i class="menu-toggle-btn fas fa-bars"></i>
+         <form action="">
+           <table class="login-control">
+             <tr>
+               <td ><label class="login-label"> Search Location</label></td>
+               <td ><label class="login-label"> Search specification</label></td>
+             </tr>
+             <tr>
+              <td><select>
+                <option value="Alappuzha" class="txtb">Alappuzha</option>
+                <option value="Alappuzha" class="txtb">kottayam</option>
+                <option value="Alappuzha" class="txtb">Idukki</option>
+                <option value="Alappuzha" class="txtb">Thiruvanthapuram</option>
+                <option value="Alappuzha" class="txtb">Kollam</option>
+                <option value="Alappuzha" class="txtb">Ernakulam</option>
+                <option value="Alappuzha" class="txtb">malappuram</option>
+                <option value="Alappuzha" class="txtb">palakkad</option>
+                <option value="Alappuzha" class="txtb">Kozhikode</option>
+                <option value="Alappuzha" class="txtb">Wayanad</option>
+                <option value="Alappuzha" class="txtb"></option>
+
+                <option value="Alappuzha" class="txtb">Alappuzha</option>
+
+                <option value="Alappuzha" class="txtb">Alappuzha</option>
+                <option value="Alappuzha" class="txtb">Alappuzha</option>
+
+
+              
+              </select></td>
+               <td><input type="text" class="txtb"></td>
+               <td ><input type="submit" value="login" class="txtb"></td>
+               
+             </tr>
+           </table>
+         </form>
+
+       </div>
+
+     </div>
+     <div id="navigation">
+       <h2> Categories</h2>
+       <div class="cat">
+        <a href="#"><i class="fas fa-home home"></i> Home</a>
+        <a href="#"><i class="fal fa-island-tropical"></i> Land</a>
+        <a href="#"><i class="fas fa-store-alt"></i> Shop</a>
+         
+       </div>
+     </div>
+     <div id="footer">
+      <div class="footer-container">
+        
+          <div class="social-media">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+          <p class="rights-text">© 2020 Created By rentItt All Rights Reserved.</p>
+        </div>
+     </div>
+   </div>
+  </body>
+</html>
